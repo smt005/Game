839a488f74393f6ef371ba155e58c70d5282ad1d
Коптяев Александр <a.koptyaev@playrix.com>
2020-10-07 19:19:47 +0300

    PhysX, реализация класса Не полный

--- Source/PhysX/MapPhysX.cpp
+++ Source/PhysX/MapPhysX.cpp
@@ -57,6 +57,16 @@ void MapPhysX::init()
 
 	if (!_mapGame) {
 		_mapGame = new Map(mapFileName);
+
+		for (int i = 0; i < 100; ++i) {
+			_mapGame->addObjectToPos("Box1", glm::vec3(
+				1.0f + std::cos((float)i / 5.0f) * 5.0f,
+				1.0f + std::sin((float)i / 5.0f) * 5.0f,
+				((float)i * 1.0f) + 10.0f
+			)).setTypeActorPhysics(Engine::Physics::Type::CONVEX);
+		}
+
+		Draw::setClearColor(_mapGame->getRed(), _mapGame->getGreen(), _mapGame->getBlue(), _mapGame->getAlpha());
 	}
 
 	_greed = new Greed(100.0f, 10.0f);
@@ -134,125 +144,15 @@ void MapPhysX::initCallback()
 }
 
 void MapPhysX::initPhysic() {
-	if (Engine::initPhysics()) {
-		if (Engine::createScene()) {
-			{ // Triangle
-				Object* triangleObject = _mapGame->getObjectByName("Triangle");
-				if (triangleObject) {
-					Model& model = triangleObject->getModel();
-					//const Shape& shape = model.getShape();
-					Mesh& mesh = model.getMesh();
-					glm::vec3 vPos = triangleObject->getPos();
-					float pos[] = { vPos.x, vPos.y, vPos.z };
-
-					if (Engine::createActorTriangle(&mesh, pos)) {
-						help::log("Game: physics is create.");
-					}
-					else {
-						help::log("Game: physics is create FAIL.");
-					}
-				}
-			}
-
-			/*for (int i = 0; i < 4; ++i)
-			{ // Box
-				//float rect[] = { 1.0f, 1.0f, 3.0f };
-				//float pos[] = { 0.0f, 0.0f, 10.0f * i };
-
-				float xR = help::random_f(0.5f, 5.0f, 100.0f);
-				float yR = help::random_f(0.5f, 5.0f, 100.0f);
-				float zR = help::random_f(0.5f, 5.0f, 100.0f);
-
-				float rect[] = { xR, yR, zR };
-
-				float x = 1.0f + std::cos((float)i / 50.0f) * 0.1f;
-				float y = 1.0f + std::sin((float)i / 50.0f) * 0.1f;
-				float z =  ((float)i * 5.0f)  + 50.0f;
-
-				float pos[] = { x, y, z };
-
-				Engine::createActorBox(rect, pos);
-			}*/
-
-			{ // Convex
-				Object* object = _mapGame->getObjectByName("Box1");
-				if (object) {
-					Model& model = object->getModel();
-					Mesh& mesh = model.getMesh();
-					glm::vec3 vPos = object->getPos();
-					float pos[] = { vPos.x, vPos.y, vPos.z };
-
-					Engine::ActorPhyscs* actorPhyscs = Engine::createActorConvex(&mesh, pos);
-					if (actorPhyscs) {
-						object->_actorPhyscs = actorPhyscs;
-					}
-				}
-			}
-
-			{ // Convex
-				Object* object = _mapGame->getObjectByName("Sphere");
-				if (object) {
-					Model& model = object->getModel();
-					Mesh& mesh = model.getMesh();
-					glm::vec3 vPos = object->getPos();
-					float pos[] = { vPos.x, vPos.y, vPos.z };
-
-					Engine::ActorPhyscs* actorPhyscs = Engine::createActorConvex(&mesh, pos);
-					if (actorPhyscs) {
-						object->_actorPhyscs = actorPhyscs;
-					}
-				}
-			}
-
-			{ // Convex
-				Object* object = _mapGame->getObjectByName("SphereBox");
-				if (object) {
-					Model& model = object->getModel();
-					Mesh& mesh = model.getMesh();
-					glm::vec3 vPos = object->getPos();
-					float pos[] = { vPos.x, vPos.y, vPos.z };
-
-					Engine::ActorPhyscs* actorPhyscs = Engine::createActorConvex(&mesh, pos);
-					if (actorPhyscs) {
-						object->_actorPhyscs = actorPhyscs;
-					}
-				}
-			}
-
-			{ // Convex
-				Object* object = _mapGame->getObjectByName("Sphere_01");
-				if (object) {
-					Model& model = object->getModel();
-					Mesh& mesh = model.getMesh();
-					glm::vec3 vPos = object->getPos();
-					float pos[] = { vPos.x, vPos.y, vPos.z };
-
-					Engine::ActorPhyscs* actorPhyscs = Engine::createActorConvex(&mesh, pos);
-					if (actorPhyscs) {
-						object->_actorPhyscs = actorPhyscs;
-					}
-				}
-			}
-
-			for (int i = 0; i < 1000; ++i) {
-				float x = 1.0f + std::cos((float)i / 50.0f) * 15.1f;
-				float y = 1.0f + std::sin((float)i / 50.0f) * 15.1f;
-				float z = ((float)i * 1.0f) + 10.0f;
-
-				float pos[] = { x, y, z };
-				glm::vec3 vPos(pos[0], pos[1], pos[2]);
-
-				//Object& object = _mapGame->addObjectToPos("Car");
-				Object& object = _mapGame->addObjectToPos("Box1");
-				Mesh& mesh = object.getModel().getMesh();
-
-				Engine::ActorPhyscs* actorPhyscs = Engine::createActorConvex(&mesh, pos);
-				if (actorPhyscs) {
-					object._actorPhyscs = actorPhyscs;
-				}
-			}	
-		}
+	if (!Engine::Physics::init()) {
+		return;
 	}
+
+	if (Engine::Physics::createScene()) {
+		return;
+	}
+
+	_mapGame->initPhysixs();
 }
 
 bool MapPhysX::load()
@@ -293,18 +193,11 @@ void MapPhysX::save()
 
 void MapPhysX::update()
 {
-	if (Engine::updateScene(Engine::Core::deltaTime())) {
-		for (auto object : _mapGame->objects) {
-			if (!object->_actorPhyscs) {
-				continue;
-			}
-
-			Position pos = Engine::getActorPos(object->_actorPhyscs);
-			object->setPos(pos);
-		}
-	}
-
 	if (_mapGame) {
+		if (Engine::Physics::updateScene(Engine::Core::deltaTime())) {
+			_mapGame->updatePhysixs();
+		}
+
 		_mapGame->action();
 	}
 }
